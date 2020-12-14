4d0e8870f2544ec2e580d6f223a5c751bbc393d3
Novia Ramadani <noviaramadani0@gmail.com>
2020-12-14 22:59:43 +0700

    feat: add fitur 15 and 17

--- sirekrutmen/src/main/java/apap/tugaskelompok/sirekrutmen/service/UserRestService.java
+++ sirekrutmen/src/main/java/apap/tugaskelompok/sirekrutmen/service/UserRestService.java
@@ -1,7 +1,12 @@
 package apap.tugaskelompok.sirekrutmen.service;
 
+import apap.tugaskelompok.sirekrutmen.rest.GajiBaseResponse;
+import apap.tugaskelompok.sirekrutmen.rest.PegawaiBaseResponse;
+
 public interface UserRestService {
 	
 	String aFunction();
+	PegawaiBaseResponse getDataPegawai(String userName);
+	GajiBaseResponse[] getStatistikGaji();
 	
 }
