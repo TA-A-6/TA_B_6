8b84fb414363043945d26ee9e5f54dba12900036
Muhamad Rian Alpajirin <ryan.cyborg205@gmail.com>
2020-12-26 22:59:16 +0700

    deployment

--- sirekrutmen/src/main/java/apap/tugaskelompok/sirekrutmen/security/UserDetailsServiceImpl.java
+++ sirekrutmen/src/main/java/apap/tugaskelompok/sirekrutmen/security/UserDetailsServiceImpl.java
@@ -14,11 +14,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import apap.tugaskelompok.sirekrutmen.model.*;
 import apap.tugaskelompok.sirekrutmen.repository.*;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService{
 
 	@Autowired
